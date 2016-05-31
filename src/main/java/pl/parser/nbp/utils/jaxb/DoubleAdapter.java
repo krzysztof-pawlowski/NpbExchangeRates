41ba91b9e80717a4a60b20ddcbac45869d8b2606
Krzysztof Pawlowski <krzysztof.pawlowski@gmail.com>
2016-05-31 21:45:30 +0200

    handling nulls in jaxb adapters

--- src/main/java/pl/parser/nbp/utils/jaxb/DoubleAdapter.java
+++ src/main/java/pl/parser/nbp/utils/jaxb/DoubleAdapter.java
@@ -8,10 +8,16 @@ import javax.xml.bind.annotation.adapters.XmlAdapter;
  */
 public class DoubleAdapter extends XmlAdapter<String, Double> {
     @Override public Double unmarshal(String v) throws Exception {
+        if (v == null) {
+            return null;
+        }
         return Double.parseDouble(v.replace(',', '.'));
     }
 
     @Override public String marshal(Double v) throws Exception {
+        if (v == null) {
+            return null;
+        }
         return v.toString().replace('.', ',');
     }
 }
--- src/main/java/pl/parser/nbp/utils/jaxb/LocalDateAdapter.java
+++ src/main/java/pl/parser/nbp/utils/jaxb/LocalDateAdapter.java
@@ -8,10 +8,16 @@ import java.time.LocalDate;
  */
 public class LocalDateAdapter extends XmlAdapter<String, LocalDate> {
     public LocalDate unmarshal(String v) throws Exception {
+        if (v == null) {
+            return null;
+        }
         return LocalDate.parse(v);
     }
 
     public String marshal(LocalDate v) throws Exception {
+        if (v == null) {
+            return null;
+        }
         return v.toString();
     }
 }
