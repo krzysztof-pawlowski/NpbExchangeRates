7c8cf8d4de4425d3e64af1843cfd2d6748cf9898
Krzysztof Pawlowski <krzysztof.pawlowski@gmail.com>
2016-05-31 20:38:34 +0200

    missing javadoc

--- src/main/java/pl/parser/nbp/downloader/model/Table.java
+++ src/main/java/pl/parser/nbp/downloader/model/Table.java
@@ -11,7 +11,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 /**
- * Created by Krzysztof Pawlowski on 29/05/16.
+ * Class representing the table from NBP rates server
  */
 @XmlType
 @XmlRootElement(name = "tabela_kursow")
@@ -23,48 +23,88 @@ public class Table {
     private LocalDate publicationDate;
     private List<TablePosition> positions;
 
+    /**
+     * Type of the table.
+     * @return type
+     */
     public String getType() {
         return type;
     }
 
+    /**
+     * Sets the type of the table.
+     * @param type type
+     */
     @XmlAttribute( name = "typ", required = true )
     public void setType(String type) {
         this.type = type;
     }
 
+    /**
+     * Gets the number of the table.
+     * @return number of the table
+     */
     public String getTableNumber() {
         return tableNumber;
     }
 
+    /**
+     * Sets the number of the table.
+     * @param tableNumber number of the table
+     */
     @XmlElement( name = "numer_tabeli" )
     public void setTableNumber(String tableNumber) {
         this.tableNumber = tableNumber;
     }
 
+    /**
+     * Gets the date of quote.
+     * @return date of quote
+     */
     public LocalDate getQuoteDate() {
         return quoteDate;
     }
 
+    /**
+     * Sets the date of quote
+     * @param quoteDate date of quote
+     */
     @XmlElement( name = "data_notowania" )
     @XmlJavaTypeAdapter(value = LocalDateAdapter.class)
     public void setQuoteDate(LocalDate quoteDate) {
         this.quoteDate = quoteDate;
     }
 
+    /**
+     * Gets the publication date.
+     * @return publication date
+     */
     public LocalDate getPublicationDate() {
         return publicationDate;
     }
 
+    /**
+     * Sets the publication date.
+     * @param publicationDate publication date
+     */
     @XmlElement( name = "data_publikacji" )
     @XmlJavaTypeAdapter(value = LocalDateAdapter.class)
     public void setPublicationDate(LocalDate publicationDate) {
         this.publicationDate = publicationDate;
     }
 
+    /**
+     * Gets the table positions.
+     * @return table positions
+     */
     public List<TablePosition> getPositions() {
         return positions;
     }
 
+    /**
+     * Sets the table positions.
+     * @param positions table positionss
+     */
     @XmlElement( name = "pozycja" )
     public void setPositions(List<TablePosition> positions) {
         this.positions = positions;
--- src/main/java/pl/parser/nbp/downloader/model/TablePosition.java
+++ src/main/java/pl/parser/nbp/downloader/model/TablePosition.java
@@ -6,7 +6,7 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 /**
- * Created by Krzysztof Pawlowski on 29/05/16.
+ * Class representing the table position from NBP rates server
  */
 public class TablePosition {
 
@@ -16,40 +16,76 @@ public class TablePosition {
     private Double buyPrice;
     private Double sellPrice;
 
+    /**
+     * Gets the name of the currency.
+     * @return name of the currency
+     */
     public String getCurrencyName() {
         return currencyName;
     }
 
+    /**
+     * Sets the name of the currency.
+     * @param currencyName name of the currency
+     * @return table position object
+     */
     @XmlElement( name = "nazwa_waluty" )
     public TablePosition setCurrencyName(String currencyName) {
         this.currencyName = currencyName;
         return this;
     }
 
+    /**
+     * Gets the scaler.
+     * @return scaler
+     */
     public String getScaler() {
         return scaler;
     }
 
+    /**
+     * Sets the scaler
+     * @param scaler scaler
+     * @return table position object
+     */
     @XmlElement( name = "przelicznik" )
     public TablePosition setScaler(String scaler) {
         this.scaler = scaler;
         return this;
     }
 
+    /**
+     * Gets the currency code.
+     * @return currency code
+     */
     public String getCurrencyCode() {
         return currencyCode;
     }
 
+    /**
+     * Sets the currency code.
+     * @param currencyCode currency code
+     * @return table position object
+     */
     @XmlElement( name = "kod_waluty" )
     public TablePosition setCurrencyCode(String currencyCode) {
         this.currencyCode = currencyCode;
         return this;
     }
 
+    /**
+     * Gets the buy price.
+     * @return buy price
+     */
     public Double getBuyPrice() {
         return buyPrice;
     }
 
+    /**
+     * Sets the buy price.
+     * @param buyPrice buy price
+     * @return table position object
+     */
     @XmlElement( name = "kurs_kupna" )
     @XmlJavaTypeAdapter(value = DoubleAdapter.class)
     public TablePosition setBuyPrice(Double buyPrice) {
@@ -57,10 +93,19 @@ public class TablePosition {
         return this;
     }
 
+    /**
+     * Get the sell price.
+     * @return sell price
+     */
     public Double getSellPrice() {
         return sellPrice;
     }
 
+    /**
+     * Sets the sell price.
+     * @param sellPrice sell price
+     * @return table position object
+     */
     @XmlElement( name = "kurs_sprzedazy" )
     @XmlJavaTypeAdapter(value = DoubleAdapter.class)
     public TablePosition setSellPrice(Double sellPrice) {
